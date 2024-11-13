5cfef6bea2e9216ac5e6243fe0608af85c1a9852
PhoebeHoku <95447747+PhoebeHoku@users.noreply.github.com>
2024-11-13 16:12:51 -0500

    working versions of pdf to json for iTextJSON.cs ApiPdfController.cs pdfLib.cshtml pdfJsToJSON.cshtml

---
+++ QuickSilver2/Models/DTOs/PDFjsJSON.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickSilver2.Models.DTOs;
+
+public class PDFjsJSON {
+    public Pages[] pages { get; set; }
+}
+
+public class Pages {
+    public int page { get; set; }
+    public string text { get; set; }
+}
---
+++ QuickSilver2/Models/DTOs/PDFlibJSON.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickSilver2.Models.DTOs;
+
+public class PDFlibJSON {
+    public LibMetadata metadata { get; set; }
+}
+
+public class LibMetadata {
+    public string title { get; set; }
+    public string author { get; set; }
+    public string subject { get; set; }
+    public string keywords { get; set; }
+}
